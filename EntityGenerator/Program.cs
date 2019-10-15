6df294fa0e6f417a259202539d337c5496ba9fb8
d7p4n4 <bercikezs199999999@gmail.com>
2019-10-15 14:32:01 +0100

    1.20191015.1

--- EntityGenerator/Program.cs
+++ EntityGenerator/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.CodeDom.Providers.DotNetCompilerPlatform;
+using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Text;
 
 namespace EntityGenerator
 {
@@ -7,8 +10,37 @@ namespace EntityGenerator
     {
         static void Main(string[] args)
         {
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.  
+            var csc = new CSharpCodeProvider();
+            var cc = csc.CreateCompiler();
+            CompilerParameters cp = new CompilerParameters();
+
+            cp.ReferencedAssemblies.Add("mscorlib.dll");
+            cp.ReferencedAssemblies.Add("System.dll");
+            StringBuilder sb = new StringBuilder();
+
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"d:\Server\Visual_studio\GeneratedClasses\GeneratedClasses\PersonPersistentPreProcessed.cs");
+            while ((line = file.ReadLine()) != null)
+            {
+                sb.Append(line);
+                counter++;
+            }
+
+            // The string can contain any valid c# code
+            // "results" will usually contain very detailed error messages
+            var results = csc.CompileAssemblyFromSource(cp, sb.ToString());
+            System.Reflection.Assembly _assembly = results.CompiledAssembly;
+            Type[] _types = _assembly.GetTypes();
+            Type eType = _assembly.GetType("Foo.MyClass");
+
+            file.Close();
+
             List<Type> list = new List<Type>();
-            list.Add(typeof(TestClass));
+            list.Add(eType);
 
             List<string> folders = new List<string>();
             folders.Add("Generated");
@@ -23,15 +55,16 @@ namespace EntityGenerator
 
                 values.Add(name, vName);
             }
-
+            /*
             Dictionary<string, string> extensions = new Dictionary<string, string>();
             extensions.Add("EntityFramework", "6.2.0");
-            
+            */
             Generator.contextGenerate(list, "TestClass", "TestDb", "EntityGenerator", "Template", "cs");
-            Generator.extensionGenerator("EntityGenerator", "TemplateEntityExtensionNETCore", "cs", "Exe", "netcoreapp2.2", folders, extensions);
-            Generator.programGenerator("TemplateSaveProgram", "cs", "EntityGenerator", "TestClassAontext", values);
+            /*Generator.extensionGenerator("EntityGenerator", "TemplateEntityExtensionNETCore", "cs", "Exe", "netcoreapp2.2", folders, extensions);
+            Generator.programGenerator("TemplateSaveProgram", "cs", "EntityGenerator", "TestClassContext", values);
 
             Generator.generateEntityMethods("TemplateEntityMethods", "cs", "EntityGenerator", typeof(TestClassContext), list);
+        */
         }
     }
 }
---
+++ EntityGenerator/ReadLines.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using Microsoft.CodeDom.Providers.DotNetCompilerPlatform;
+using System.CodeDom.Compiler;
+
+namespace EntityGenerator
+{
+    public class ReadLines
+    {
+        public static Type readIn() {
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.  
+            var csc = new CSharpCodeProvider();
+            var cc = csc.CreateCompiler();
+            CompilerParameters cp = new CompilerParameters();
+
+            cp.ReferencedAssemblies.Add("mscorlib.dll");
+            cp.ReferencedAssemblies.Add("System.dll");
+            StringBuilder sb = new StringBuilder();
+
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"d:\Server\Visual_studio\GeneratedClasses\GeneratedClasses\PersonPersistentPreProcessed.cs");
+            while ((line = file.ReadLine()) != null)
+            {
+                sb.Append(line);
+                counter++;
+            }
+
+            // The string can contain any valid c# code
+            // "results" will usually contain very detailed error messages
+            var results = csc.CompileAssemblyFromSource(cp, sb.ToString());
+            System.Reflection.Assembly _assembly = results.CompiledAssembly;
+            Type[] _types = _assembly.GetTypes();
+            Type eType = _assembly.GetType("Foo.MyClass");
+
+            file.Close();
+
+            return eType;
+        }
+    }
+}
